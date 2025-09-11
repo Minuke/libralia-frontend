f86f9ba8b16df4f717b216b45cd70fbf4381c3f9
Minuke <nakamadeath@gmail.com>
2025-09-11 21:27:09 +0200

    refactor book interfaces; restructure and expand book-related data models for improved clarity and functionality

--- src/app/features/dashboard/entities/interfaces/books.interface.ts
+++ src/app/features/dashboard/entities/interfaces/books.interface.ts
@@ -1,6 +1,66 @@
-export interface Book {
-  id: number;
+export interface PageRequest {
+  order: number;
+  content: string;
+}
+
+export interface PageResponse {
+  id: string;
+  order: number;
+  content: string;
+}
+
+export interface ChapterRequest {
   title: string;
-  cover: string;
-  authorId: number;
+  order?: number;
+  pages?: PageRequest[];
+}
+
+export interface ChapterResponse {
+  id: string;
+  title: string;
+  order: number;
+  pages: PageResponse[];
+}
+
+export interface BookResponse {
+  id: string;
+  title: string;
+  author: string;
+  description: string;
+  tags: string[];
+  chapters: ChapterResponse[];
+}
+
+export interface CreateBookRequest {
+  title: string;
+  author: string;
+  description: string;
+  tags?: string[];
+  chapters?: ChapterRequest[];
+}
+
+export interface UpdateBookRequest {
+  title: string;
+  author: string;
+  description: string;
+  tags?: string[];
+  chapters?: ChapterRequest[];
+}
+
+export interface PatchedUpdateBookRequest {
+  title?: string;
+  author?: string;
+  description?: string;
+  tags?: string[];
+  chapters?: ChapterRequest[];
+}
+
+export interface PaginatedBookResponse {
+  count: number;
+  page: number;
+  page_size: number;
+  pages: number;
+  has_next: boolean;
+  has_previous: boolean;
+  results: BookResponse[];
 }
