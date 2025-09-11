97f9450ad3c5fb519b1d8bec8b6b45b07791750f
Minuke <nakamadeath@gmail.com>
2025-09-11 21:15:09 +0200

    refactor profile page layout; update grid structure and integrate user books component

--- src/app/features/dashboard/pages/profile-page/profile-page.component.ts
+++ src/app/features/dashboard/pages/profile-page/profile-page.component.ts
@@ -2,11 +2,12 @@ import { Component, computed, inject } from '@angular/core';
 import { AuthService } from '@core/services/auth.service';
 import { DataUserComponent } from '@features/dashboard/components/data-user/data-user.component';
 import { WelcomeMessageComponent } from '@features/dashboard/components/welcome-message/welcome-messasge.component';
+import { UserBooksComponent } from '../../components/user-books/user-books.component';
 
 @Component({
   selector: 'app-profile-page',
   standalone: true,
-  imports: [WelcomeMessageComponent, DataUserComponent],
+  imports: [WelcomeMessageComponent, DataUserComponent, UserBooksComponent],
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.scss'
 })
