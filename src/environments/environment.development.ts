42b85ed315caa270e848c7a08edf3bfdef63d47a
Minuke <nakamadeath@gmail.com>
2025-09-13 16:23:13 +0200

    stop tracking environments folder

--- src/environments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8000/api',
-  booksUrl: 'http://localhost:8000/books',
-  googleClientId: "1050138828075-i0hejlpr7k30bhbe00su58j9612uru4b.apps.googleusercontent.com",
-  googleCallbackUri: "http://localhost:4200/auth/google/callback"
-};
